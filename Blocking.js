e2334def1cde026d02763d0fb19a2eb383fd1851
karthik-md-06 <karthik.md@polynomial.ai>
2023-02-02 21:54:18 +0530

    changes made

--- Blocking.js
+++ Blocking.js
@@ -1,4 +1,13 @@
 var fs=require("fs");
 var data=fs.readFileSync("text.txt");
 console.log(data.toString());
-console.log("end here")
+function product(n)
+{
+  var res=1;
+  for(var i=0;i<n.length;i++)
+    {
+        res*=Number(n[i]);
+    }
+    return res;
+}
+console.log(product([5,9,9]));
